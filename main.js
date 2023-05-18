cdcfa5339a4f77cb9b376c47b04bcf7f972c5cd4
michael12345678 <michaelferreiradesousa@gmail.com>
2023-05-18 12:40:18 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#cpf').mask('000.000.000-00', {
+        reverse: true,
+        placeholder: '000.000.000-00'
+    });
+
+
+    $('#cep').mask('00000-000', {
+        reverse: true,
+        placeholder: '00000 - 000'
+    });
+
+})
